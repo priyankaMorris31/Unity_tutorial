2ceb8684abe5913af0911869607db3c2654801dc
priyankaMorris31 <priyankabteck2020@gmail.com>
2026-01-15 00:23:34 +0530

    Third commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    // this is player Scontrol
+    private float speed = 5.0f;
+    private float turnSpeed = 10.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    public PlayerController(float turnSpeed)
+    {
+        this.turnSpeed = turnSpeed;
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,7 +21,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // this is player input 
+        horizontalInput = Input.GetAxis("Horizontal");
         // Move the vichcle Forword
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
